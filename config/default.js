1907aabe10beaf173540dd08f0eca711f8894738
Enieber <eniebercunha@gmail.com>
2015-11-24 06:02:15 -0200

    save dependenci

--- config/default.js
+++ config/default.js
@@ -1,12 +1,8 @@
 module.exports = {
   github: {
-    clientID: '', // Go to github.com, create an application and add clientID
-    clientSecret: '' // Go to github.com, create an application and add clientSecret
-  },
+ },
   twitter: {
-    consumerKey: '', // Go to apps.twitter.com, create an application and add consumerKey
-    consumerSecret: '' // Go to apps.twitter.com, create an application and add consumerSecret
-  },
+ },
   url: '127.0.0.1',
   ports: {
     http: 8000
